5c37e55351a193f3193ac79f636ba08df68c0adc
sosorio18 <84107233+sosorio18@users.noreply.github.com>
2021-10-02 13:47:36 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -19,3 +19,18 @@ total_summary <- susp_coil %>% summarize(Mean = mean(PSI), Median = median(PSI),
 
 #lot_summary
 lot_summary <- susp_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+
+## Deliverable 3
+t.test(susp_coil$PSI, mu=1500)
+
+
+# psi for each lot
+Lot1= subset(susp_coil, Manufacturing_Lot== 'Lot1')
+Lot2= subset(susp_coil, Manufacturing_Lot== 'Lot2')
+Lot3= subset(susp_coil, Manufacturing_Lot== 'Lot3')
+
+
+t.test(Lot1$PSI, mu=1500)
+t.test(Lot2$PSI, mu=1500)
+t.test(Lot3$PSI, mu=1500)
