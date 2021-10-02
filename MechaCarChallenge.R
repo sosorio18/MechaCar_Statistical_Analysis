0a0c67a96b297338b13cb00ed97d007ce0e34b97
sosorio18 <84107233+sosorio18@users.noreply.github.com>
2021-10-02 13:31:06 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,13 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 # pvalue and r-squared
 # p-value: 5.35e-11
 # Adjusted R-squared:  0.6825 
+
+## Deliverable 2
+# import data
+susp_coil <- read.csv(file='Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+# total_summary 
+total_summary <- susp_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+#lot_summary
+lot_summary <- susp_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
